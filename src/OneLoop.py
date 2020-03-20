eb3a76a3365d7cf491c6e778474ecc563a6614e5
xuechao <xuechaofelix@hotmail.com>
2020-03-20 10:22:36 +0800

    upload

---
+++ src/OneLoop.py
@@ -0,0 +1,11 @@
+from z3 import *
+
+L = [
+lambda x :(True and (x[0] > 0)),
+lambda x :[(x[0] - x[1]), (x[1] + 1), ],
+2,
+0,
+lambda x :[( x[0]- x[1]), ( x[1]+ 1), ],
+lambda x :(And( True, ( x[0]> 0))),
+False,
+]
